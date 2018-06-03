9df652b9e76e94f7497e5b6e51a797817ae06bff
Simon Praetorius <simon@praetorius.me>
2018-06-03 16:32:05 +0000

    Simple spotify remote for raspberry pi GPIO

---
+++ README.md
@@ -0,0 +1,8 @@
+# Spotify Remote
+
+## Setup
+
+```
+sudo apt install python-gpiozero
+pip install -U python-dotenv
+pip install git+https://github.com/plamere/spotipy.git --upgrade
---
+++ spotifyRemote.py
@@ -0,0 +1,109 @@
+from gpiozero import LED, Button
+import spotipy, spotipy.util
+from dotenv import load_dotenv, find_dotenv
+from signal import pause
+
+# Interpret environment variables
+load_dotenv(find_dotenv())
+
+# Button inputs
+previous_track = Button(21)
+play = Button(20)
+next_track = Button(16)
+shuffle = Button(12)
+repeat = Button(25)
+favorite = Button(24)
+
+# LED outputs
+playing = LED(26)
+shuffling = LED(19)
+repeating = LED(13)
+
+
+def login_to_spotify():
+	print "Logging in to Spotify..."
+
+	token = spotipy.util.prompt_for_user_token(
+		username = "qeucjthqoqgkg3tcpxmwhbrpw",
+		scope = "user-modify-playback-state,user-read-playback-state,user-read-currently-playing",
+		redirect_uri = "http://localhost/"
+	)
+
+	spotify = spotipy.Spotify(auth = token)
+
+	user = spotify.current_user()
+	print "Logged in as " + user["id"]
+
+	return spotify
+
+
+def refresh_leds():
+	currentPlayback = spotify.current_playback()
+
+	if currentPlayback["is_playing"]:
+		playing.on()
+	else:
+		playing.off()
+
+	if currentPlayback["shuffle_state"]:
+		shuffling.on()
+	else:
+		shuffling.off()
+
+	if currentPlayback["repeat_state"] == "off":
+		repeating.off()
+	else:
+		repeating.on()
+
+
+def toggle_shuffle():
+	if shuffling.is_lit:
+		spotify.shuffle(False)
+	else:
+		spotify.shuffle(True)
+
+	refresh_leds()
+
+
+def toggle_repeat():
+	if repeating.is_lit:
+		spotify.repeat("off")
+	else:
+		spotify.repeat("context")
+
+	refresh_leds()
+
+
+def toggle_play():
+	if playing.is_lit:
+		spotify.pause_playback()
+	else:
+		spotify.start_playback()
+
+	refresh_leds()
+
+
+def play_favorite():
+	print "This is your task"
+
+
+# Main program starts here
+
+# Login to spotify
+spotify = login_to_spotify()
+
+# Refresh states of LEDs
+refresh_leds()
+
+# Define actions for buttons
+play.when_pressed = toggle_play
+next_track.when_pressed = spotify.next_track
+previous_track.when_pressed = spotify.previous_track
+
+shuffle.when_pressed = toggle_shuffle
+repeat.when_pressed = toggle_repeat
+
+favorite.when_pressed = play_favorite
+
+print "Waiting for input..."
+pause()
